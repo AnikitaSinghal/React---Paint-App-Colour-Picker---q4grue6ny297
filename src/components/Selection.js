979d26e7e44d70dc2409fafb3d47e45ba9101c0a
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-22 14:24:29 +0530

    completed

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -3,11 +3,7 @@ function Selection(){
     return(
         <div class="fix-box" onClick={()=>
             {applyColor}}>
-                <div class="subheading">
-                    <h2>
-                        Selection
-                    </h2>
-                </div>
+                 
             </div>
 )
 }
