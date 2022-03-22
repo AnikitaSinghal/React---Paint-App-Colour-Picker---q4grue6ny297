1202c1d440482c3246af8337731025344f1c12bd
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-22 14:16:32 +0530

    completed

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,8 +1,14 @@
 import React from "react";
-function Selection(){
+function Selection(props){
     return(
-        <div class="fix-box" onClick={applyColor}>
-        </div>
+        <div class="fix-box" onClick={()=>
+            {applyColor}}>
+                <div class="subheading">
+                    <h2>
+                        Selection
+                    </h2>
+                </div>
+            </div>
 )
 }
 export default Selection;
