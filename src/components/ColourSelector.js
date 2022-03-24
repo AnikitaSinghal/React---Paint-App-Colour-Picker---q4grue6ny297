d9406146ab0d2470a453f450475b2926a2949c7d
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-24 17:57:58 +0530

    completed

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -4,8 +4,8 @@ const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config;
   return (
-    <button className={config.className} onClick={() => selectNextBackground({background: background})}>
-      {config.label}
+    <button className={config.classname} onClick={() => selectNextBackground({background: background})}>
+      {config.lable}
     </button>
   )
 }
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,13 +1,9 @@
 import React from "react";
 function Selection(){
     return(
-        <div class="fix-box" style={color} 
-        onClick={(e)=>
-        applyColor(setColor)}>
-        <h2 class="subheading">
-            Selection
-            </h2>      
-            </div>
-)
+        <div className="fix-box" style={color} onClick={() => applyColor(setColor)}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+);
 }
 export default Selection;
